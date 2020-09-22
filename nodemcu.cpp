30c92de2a9b0777f0079773930a39befa52a86c8
unknown <dishendramishra@gmail.com>
2020-09-22 14:56:29 +0530

    added nodemcu sketch

---
+++ nodemcu.cpp
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+const byte interruptpin = 0;
+void ICACHE_RAM_ATTR ISRoutine();
+
+int shutter = 0; // 0 is closed, 1 is open
+String reply;
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(interruptpin, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN,HIGH);
+    attachInterrupt(digitalPinToInterrupt(interruptpin), ISRoutine, FALLING);
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3D for 128x64
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ;
+    }
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+}
+
+void loop()
+{
+    if (shutter == 1)
+    {
+        display.clearDisplay();
+        display.setCursor(0, 10);
+        display.println("OPEN");
+        display.display();
+    }
+    else
+    {
+        display.clearDisplay();
+        display.setCursor(0, 10);
+        display.println("CLOSED");
+        display.display();
+    }
+
+    if (Serial.available() > 0) {
+        String msg = Serial.readString();
+        msg.trim();
+
+        if( msg == "close-shutter"){
+            Serial.println("Closing shutter .........");
+            shutter = 0;
+        } 
+        else if( msg == "open-shutter"){
+            Serial.println("Opeing shutter .........");
+            shutter = 1;
+        }
+        else if(msg == "status"){
+            reply = (shutter == 1) ? "open" : "closed";
+            Serial.println(reply);
+        }
+
+    }
+}
+
+void ISRoutine()
+{
+    int value = digitalRead(LED_BUILTIN);
+    digitalWrite(LED_BUILTIN, !value);
+    if (value == HIGH)
+        shutter = 1;
+    else
+        shutter = 0;
+}
