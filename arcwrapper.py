88b2214a035751643be8bb43bc65dc56e9aaf036
unknown <dishendramishra@gmail.com>
2021-08-25 14:42:39 +0530

    added

--- arcwrapper.py
+++ arcwrapper.py
@@ -6,24 +6,25 @@ from time import sleep
 class ArcWrapper():
 
     def __init__(self):
-        os.chdir("api")
-        self.process = Popen("ArcAPI35Ex_1.exe", stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        # os.chdir("api")
+        self.process = Popen("./api/ArcAPI35Ex_1.exe", stdin=PIPE, stdout=PIPE, stderr=STDOUT)
 
-    def write_stdin(self, cmd):
-        cmd = str(cmd)+"\n"
+    def write_stdin(self, cmd, delimiter="\n"):
+        cmd = str(cmd)+delimiter
         self.process.stdin.write(cmd.encode())
         self.process.stdin.flush()
 
-    def read_stdout(self):
+    def read_stdout(self, end_string="Enter any key"):
       for line in self.process.stdout:
-        line = line.decode("utf-8")
+        line = line.decode("utf-8").strip()
         print(line)
 
-        if line.startswith("   Enter any key to"):
+        if line.startswith(end_string):
           break
 
     def kill(self):
       self.write_stdin("0")
+      self.read_stdout()
       self.process.kill()
 
     def apply_setup(self):
@@ -31,46 +32,53 @@ class ArcWrapper():
         self.read_stdout()
         self.write_stdin("")
 
-    def take_exposure(self, exp_time, fits_filename, shutter_cfg):
-        self.write_stdin("2")
-        self.write_stdin(exp_time)
+    def take_exposure(self, exp_time, shutter_cfg, fits_filename):
+        self.write_stdin("2", " ")
+        self.write_stdin(exp_time," ")
+        self.write_stdin(shutter_cfg," ")
         self.write_stdin(fits_filename)
-        self.write_stdin(shutter_cfg)
-
-        # self.read_stdout()
-        # self.write_stdin("")
+        self.read_stdout()
+        self.write_stdin("")
 
     def open_shutter(self):
         self.write_stdin("3")
+        self.read_stdout()
         self.write_stdin("")
 
     def close_shutter(self):
         self.write_stdin("4")
+        self.read_stdout()
         self.write_stdin("")
     
     def poweroff(self):
         self.write_stdin("5")
+        self.read_stdout()
         self.write_stdin("")
     
     def poweron(self):
         self.write_stdin("6")
+        self.read_stdout()
         self.write_stdin("")
 
     def clear_camera_array(self):
         self.write_stdin("7")
+        self.read_stdout()
         self.write_stdin("")
 
     def is_open(self):
         self.write_stdin("8")
+        self.read_stdout()
         self.write_stdin("")
 
-# if __name__ == "__main__":
-#   arc = ArcWrapper()
-#   arc.apply_setup()
-# #   sleep(1)
-#   arc.take_exposure(0,"test.fits",0)
+if __name__ == "__main__":
+  arc = ArcWrapper()
+  arc.apply_setup()
 # #   arc.open_shutter()
-# #   arc.close_shutter()
-#   sleep(0.5)
-# #   arc.kill()
 # #   sleep(1)
+#   arc.take_exposure(0,0,"test.fits")
+#   arc.kill()
+  arc.open_shutter()
+  arc.close_shutter()
+#   sleep(0.5)
+  arc.kill()
+#   sleep(1)
